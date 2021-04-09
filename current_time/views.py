22b88a27500e807d755f6d44db257c0731b3ebc4
Garrett Schmahl <gschmahl@pm.me>
2021-04-09 14:54:30 -0700

    removed extra var

--- current_time/views.py
+++ current_time/views.py
@@ -3,7 +3,6 @@ from time import gmtime, strftime
     
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", gmtime()),
-        "hour": 5
+        "days": strftime("%Y-%m-%d %H:%M %p", gmtime()),
     }
     return render(request,'index.html', context)
