82e3dfe8f686b149867f9c2f329a77e688606931
Garrett Schmahl <gschmahl@pm.me>
2021-04-14 16:43:20 -0700

    users shell complete

---
+++ django_projects/033-users_shell/users_shell/main_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+    
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateTimeField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
