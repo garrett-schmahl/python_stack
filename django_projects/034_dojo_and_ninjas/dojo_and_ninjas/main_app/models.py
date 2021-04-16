1f896dc3e6eeaef8b2f381a1ddbbd8c663cd5556
Garrett Schmahl <gschmahl@pm.me>
2021-04-15 18:00:49 -0700

    completed dojo with ninjas

--- django_projects/034_dojo_and_ninjas/dojo_and_ninjas/main_app/models.py
+++ django_projects/034_dojo_and_ninjas/dojo_and_ninjas/main_app/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Dojo(models.Model):
     dojo_name = models.CharField(max_length=255)
+    dojo_city = models.CharField(max_length=255)
+    dojo_state = models.CharField(max_length=255)
+    desc = models.TextField(default="old dojo")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -11,6 +14,4 @@ class Ninja(models.Model):
     dojo = models.ForeignKey(Dojo, related_name="ninjas", on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    
 
--- django_projects/034_dojo_and_ninjas/dojo_and_ninjas/main_app/views.py
+++ django_projects/034_dojo_and_ninjas/dojo_and_ninjas/main_app/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
 def index(request):
-    return HttpResponse("hola")
+    context = {
+        "dojos": Dojo.objects.all()
+    }
+    return render(request, "index.html", context)
+
+
+def process_new(request):
+    if request.POST["submit_type"] == "dojo":
+        Dojo.objects.create(
+            dojo_name=request.POST['dojo_name'], 
+            dojo_city=request.POST['dojo_city'], 
+            dojo_state=request.POST['dojo_state'])
+    if request.POST["submit_type"] == "ninja":
+        Ninja.objects.create(
+            ninja_trainee=f"{request.POST['ninja_first_name']} {request.POST['ninja_last_name']}", 
+            dojo=Dojo.objects.get(id=request.POST["ninja_dojo"]))
+    return redirect('/')
+
+
+def delete(request):
+    if request.POST["delete_type"] == "dojo":
+        Dojo.objects.get(id=request.POST["delete_link"]).delete()
+    else:
+        Ninja.objects.get(id=request.POST["delete_link"]).delete()
+    return redirect('/')
