a6df2205de262d1fae05345027fb8776ea192ae8
Garrett Schmahl <gschmahl@pm.me>
2021-04-18 22:08:17 -0700

    failed tablesort

---
+++ django_projects/035_books_and_authors/books_and_authors/main_app/static/js/main.js
@@ -0,0 +1,31 @@
+// $(document).ready(function() {
+//     console.log("loaded")
+//     function sortTable() {
+//         var table, rows, switching, i, x, y, shouldSwitch;
+//         table = document.getElementById("data_table");
+//         switching = true;
+//         while (switching) {
+//             switching = false;
+//             rows = table.rows;
+//             for (i = 1; i < (rows.length - 1); i++) {
+//                 shouldSwitch = false;
+//                 x = rows[i].getElementsByTagName("TD")[0];
+//                 y = rows[i + 1].getElementsByTagName("TD")[0];
+//                 if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+//                     shouldSwitch = true;
+//                     break;
+//                 }
+//             }
+//             if (shouldSwitch) {
+//                 rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+//                 switching = true;
+//             }
+//         }
+//     }
+
+//     $('#data_table tr').click(function () {
+//         console.log("things are happening")
+//         sortTable()
+//     })
+
+// })
