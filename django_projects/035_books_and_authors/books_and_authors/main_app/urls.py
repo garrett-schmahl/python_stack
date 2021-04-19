6fd3799f8e798c1a0dd9492da2c11b2ee10e41aa
Garrett Schmahl <gschmahl@pm.me>
2021-04-18 21:27:27 -0700

    got page cycling working

--- django_projects/035_books_and_authors/books_and_authors/main_app/urls.py
+++ django_projects/035_books_and_authors/books_and_authors/main_app/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('<style>/<int:auth_id>/<int:book_id>/disconnect', views.delete_connection),
-    path('<style>/<int:id_num>/update', views.update_data),
-    path('<style>/<int:id_num>/connect', views.add_connection),
-    path('<style>/delete', views.delete_entry),
-    path('<style>/add', views.add),
-    path('<style>/<int:id_num>', views.details),
-    path('<style>', views.index),
+    path('<page_type>/<int:auth_id>/<int:book_id>/disconnect', views.delete_connection),
+    path('<page_type>/<int:id_num>/update', views.update_data),
+    path('<page_type>/<int:id_num>/connect', views.add_connection),
+    path('<page_type>/<int:id_num>/<direction>', views.next_item),
+    path('<page_type>/delete', views.delete_entry),
+    path('<page_type>/add', views.add),
+    path('<page_type>/<int:id_num>', views.details),
+    path('<page_type>', views.index),
 ]
--- django_projects/035_books_and_authors/books_and_authors/main_app/views.py
+++ django_projects/035_books_and_authors/books_and_authors/main_app/views.py
@@ -2,37 +2,37 @@ from django.shortcuts import render, redirect
 from .models import *
 
 
-def index(request, style="books"):
-    if style == "books":
+def index(request, page_type="books"):
+    if page_type == "books":
         pass_data = Book.objects.all()
     else:
         pass_data = Author.objects.all()
 
     context={
         "table_data": pass_data,
-        "style": style,
+        "page_type": page_type,
     }
     return render(request,"index.html",context)
 
 
-def details(request, style, id_num):
-    if style == "books":
+def details(request, page_type, id_num):
+    if page_type == "books":
         details_data = Book.objects.get(id=id_num)
         drop_down_data = Author.objects.all()
-    elif style == "authors":
+    elif page_type == "authors":
         details_data = Author.objects.get(id=id_num)
         drop_down_data = Book.objects.all()
 
     context={
         "details_data":details_data,
         "drop_down_data":drop_down_data,
-        "style": style,
+        "page_type": page_type,
     }
     return render(request,"details.html",context)
 
 
-def add(request, style):
-    if style == "books":
+def add(request, page_type):
+    if page_type == "books":
         Book.objects.create(
             title=request.POST['input_name_box'],
             description=request.POST['description_box']
@@ -43,36 +43,36 @@ def add(request, style):
             last_name=request.POST['last_name'],
             notes=request.POST['description_box']
         )
-    return redirect(f'/{style}')
+    return redirect(f'/{page_type}')
 
 
-def delete_entry(request, style):
-    if style == "books":
+def delete_entry(request, page_type):
+    if page_type == "books":
         Book.objects.get(id=request.POST["delete_id"]).delete()
     else:
         Author.objects.get(id=request.POST["delete_id"]).delete()
-    return redirect(f'/{style}')
+    return redirect(f'/{page_type}')
 
 
-def delete_connection(request, style, auth_id, book_id):
-    if style == "books":
+def delete_connection(request, page_type, auth_id, book_id):
+    if page_type == "books":
         Book.objects.get(id=book_id).authors.remove(Author.objects.get(id=auth_id))
-        return redirect(f'/{style}/{book_id}')
+        return redirect(f'/{page_type}/{book_id}')
     else:
         Author.objects.get(id=auth_id).books.remove(Book.objects.get(id=book_id))
-        return redirect(f'/{style}/{auth_id}')
+        return redirect(f'/{page_type}/{auth_id}')
 
 
-def add_connection(request, style, id_num):
-    if style == "books":
+def add_connection(request, page_type, id_num):
+    if page_type == "books":
         Book.objects.get(id=id_num).authors.add(Author.objects.get(id=request.POST['drop_down_select']))
     else:
         Book.objects.get(id=request.POST['drop_down_select']).authors.add(Author.objects.get(id=id_num))
-    return redirect(f'/{style}/{id_num}')
+    return redirect(f'/{page_type}/{id_num}')
 
 
-def update_data(request, style, id_num):
-    if style == "books":
+def update_data(request, page_type, id_num):
+    if page_type == "books":
         book_to_edit = Book.objects.get(id=id_num)
         book_to_edit.description = request.POST["description_box"]
         book_to_edit.save()
@@ -80,15 +80,36 @@ def update_data(request, style, id_num):
         author_to_edit = Author.objects.get(id=id_num)
         author_to_edit.notes = request.POST["description_box"]
         author_to_edit.save()
-    return redirect(f'/{style}/{id_num}')
+    return redirect(f'/{page_type}/{id_num}')
 
 
-def iterate_id(request, style, id_num, direction):
-    for book in Book.objects.all().order_by("id"){
-        if book.id > id_num:
-            smallestDiff = id_num-book.id
-            smalllest
-    }
-    #find the closest >
+def next_item(request, page_type, id_num, direction):
+    trigger=False
+    new_id_num = False
+    if direction == "next":
+        for book in Book.objects.all():
+            if trigger == True:
+                new_id_num= book.id
+                break
+            if book.id == id_num:
+                trigger=True
+        if new_id_num:
+            return redirect(f'/{page_type}/{new_id_num}')
+        else:
+            return redirect(f'/{page_type}/{id_num}')
+            
+    else:
+        for book in Book.objects.all()[::-1]:
+            if trigger == True:
+                new_id_num = book.id
+                break
+            if book.id == id_num:
+                trigger=True
+        if new_id_num:
+            return redirect(f'/{page_type}/{new_id_num}')
+        else:
+            return redirect(f'/{page_type}/{id_num}')
 
-    return redirect(f'/{style}/{new_id_num}')
+    
+ 
+        
