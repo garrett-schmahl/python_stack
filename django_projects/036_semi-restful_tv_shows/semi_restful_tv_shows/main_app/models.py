b27650104ac59a5572f38e23014f8b1b1b5cd126
Garrett Schmahl <gschmahl@pm.me>
2021-04-19 21:05:44 -0700

    with validation complete

--- django_projects/036_semi-restful_tv_shows/semi_restful_tv_shows/main_app/models.py
+++ django_projects/036_semi-restful_tv_shows/semi_restful_tv_shows/main_app/models.py
@@ -1,10 +1,30 @@
 from django.db import models
+from datetime import datetime
+import time
 
 class ShowManager(models.Manager):
-    def validator(self, post_data):
+    def validator(self, post_data, shows):
         errors = {}
-        if len(post_data['name'] < 1):
-            errors['length'] = "name must be at east 1 letter"
+        for show in shows:
+            if post_data['show_name_input'] == show.name:
+                errors['exists'] = "A show with that name already exists"
+        if len(post_data['show_name_input']) < 1:
+            errors['length'] = "Name must be at least 2 letters"
+        if post_data['show_description'] != "":
+            if len(post_data['show_description']) < 10:
+                errors['length'] = "Description must be at least 10 letters"
+        now = datetime.now()
+        release_date = time.strptime(post_data['show_date_input'], "%Y-%m-%d")
+        if release_date.tm_year < now.year:
+            pass
+        elif release_date.tm_year == now.year:
+            if release_date.tm_mon < now.month:
+                pass
+            elif release_date.tm_mon == now.month:
+                if release_date.tm_mday <= now.day:
+                    pass
+                else:
+                    errors['date']= "That date is in the future."
         return errors
 
 
@@ -16,10 +36,9 @@ class Network(models.Model):
 
 class Show(models.Model):
     name = models.CharField(max_length=255)
-    release_date = models.DateTimeField()
-    description = models.TextField()
+    release_date = models.DateField()
+    description = models.TextField(null=True, blank=True)
     network = models.ForeignKey(Network, related_name="shows", on_delete= models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     objects = ShowManager()
-    
--- django_projects/036_semi-restful_tv_shows/semi_restful_tv_shows/main_app/views.py
+++ django_projects/036_semi-restful_tv_shows/semi_restful_tv_shows/main_app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-# from django.contrib import messages
+from django.contrib import messages
 from .models import Network, Show
-
 
 def landing(request):
     return redirect('/shows')
@@ -22,16 +21,23 @@ def new_show(request):
 
 
 def create_show(request):
-    Show.objects.create(
-        name=request.POST['show_name_input'],
-        release_date=request.POST['show_date_input'],
-        network=Network.objects.get(id=request.POST['network_select']),
-        description=request.POST['show_description']
-    )
-    return redirect('/shows')
+    errors = Show.objects.validator(request.POST, Show.objects.all())
+    for key, value in errors.items():
+        messages.error(request, value)
+        return redirect('/shows/new')
+    else:
+        new_show=Show.objects.create(
+            name=request.POST['show_name_input'],
+            release_date=request.POST['show_date_input'],
+            network=Network.objects.get(id=request.POST['network_select']),
+            description=request.POST['show_description']
+        )
+        print(new_show.release_date)
+        return redirect(f'/shows/{new_show.id}')
 
 
 def edit_show(request, show_id):
+    current_show = Show.objects.get(id=show_id)
     context = {
         'all_networks': Network.objects.all(),
         'show_to_edit': Show.objects.get(id=show_id),
@@ -40,13 +46,19 @@ def edit_show(request, show_id):
 
 
 def update_show(request, show_id):
-    update_show = Show.objects.get(id=show_id)
-    update_show.name = request.POST['show_name_input']
-    update_show.release_date = request.POST['show_date_input']
-    update_show.network = Network.objects.get(id=request.POST['network_select'])
-    update_show.description = request.POST['show_description']
-    update_show.save()
-    return redirect('/shows')
+    errors = Show.objects.validator(request.POST, Show.objects.all())
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/shows/{show_id}/edit')
+    else:
+        update_show = Show.objects.get(id=show_id)
+        update_show.name = request.POST['show_name_input']
+        update_show.release_date = request.POST['show_date_input']
+        update_show.network = Network.objects.get(id=request.POST['network_select'])
+        update_show.description = request.POST['show_description']
+        update_show.save()
+        return redirect(f'/shows/{show_id}')
 
 
 def show_detail(request, show_id):
