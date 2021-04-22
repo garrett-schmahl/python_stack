c38226606c527ba4f899f73ae59c7fa94fe3fb09
Garrett Schmahl <gschmahl@pm.me>
2021-04-21 22:09:40 -0700

    initial commit the wall

---
+++ django_projects/038_the_wall/the_wall/user_control/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('user/create', views.create_user),
+    path('user/login', views.log_in_user),
+]
