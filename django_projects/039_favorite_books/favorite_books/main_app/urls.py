e6c962f55a4a0f8e3f8f957d4dd2569244e13329
Garrett Schmahl <gschmahl@pm.me>
2021-04-23 09:11:08 -0700

    added view_book page

--- django_projects/039_favorite_books/favorite_books/main_app/urls.py
+++ django_projects/039_favorite_books/favorite_books/main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('create', views.create_book)
+    path('create', views.create_book),
+    path('<int:book_id>/view', views.view_book),
+    path('<int:book_id>/like', views.like_book),
 ]
--- django_projects/039_favorite_books/favorite_books/main_app/views.py
+++ django_projects/039_favorite_books/favorite_books/main_app/views.py
@@ -26,3 +26,17 @@ def create_book(request):
     return redirect ('/favorite_books')
 
 
+def view_book(request, book_id):
+    if 'uuid' not in request.session:
+        return redirect("/")
+    context = {
+        "user":User.objects.get(id=request.session['uuid']),
+        "book":Book.objects.get(id=book_id)
+    }
+    return render(request, 'main_app/view_book.html', context)
+
+
+def like_book(request, book_id):
+    user = User.objects.get(id=request.session['uuid'])
+    Book.objects.get(id=new_book.id).favorite_of.add(user)
+    return redirect ('/favorite_books')
