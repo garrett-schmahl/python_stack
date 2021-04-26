93ab2068adea364505f404fa8b9892f649f442a6
Garrett Schmahl <gschmahl@pm.me>
2021-04-26 12:18:19 -0700

    4-26 algo

---
+++ algo/algo4-26.js
@@ -0,0 +1,44 @@
+//call function, dfault sum as 0
+//check if array length > 0, if yes, continue, if no, return sum
+//
+
+/* 
+  Recursively sum an arr of ints
+*/
+const nums1 = [1,2,3]
+
+function sumArr(nums){
+    if (nums.length <1) return 0
+    let addVal=nums.pop()
+    return addVal + sumArr(nums)
+}
+
+console.log(sumArr(nums1))
+
+/*****************************************************************************/
+
+/* 
+  Recursive Sigma
+  Input: integer
+  Output: sum of integers from 1 to Input integer
+*/
+
+const num1 = 5;
+const expected1 = 15;
+// Explanation: (1+2+3+4+5)
+
+const num2 = 2.5;
+const expected2 = 3;
+// Explanation: (1+2)
+
+const num3 = -1;
+const expected3 = 0;
+
+function recursiveSigma(num) {
+    if (num < 1) return 0
+    num = Math.floor(num)
+    return num + recursiveSigma(num - 1)
+  }
+console.log(recursiveSigma(num1))
+console.log(recursiveSigma(num2))
+console.log(recursiveSigma(num3))
